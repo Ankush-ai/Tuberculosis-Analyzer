0c9d56180a5e3f4870288550ab8f8ec270e30945
Ankush Srivastava <ankushsri007@gmail.com>
2024-05-20 21:23:55 +0530

    modified main file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from faker import Faker
-import requests
 
 fake = Faker()
 
@@ -36,6 +35,36 @@ class TuberculosisAnalyzer:
         }
         return model_info
 
+def print_logs():
+    logs = [
+        "9.9s\t1\t[NbConvertApp] Converting notebook script.ipynb to html",
+        "14.6s\t2\t[NbConvertApp] Executing notebook with kernel: python3",
+        "300.5s\t3\t2018-11-24 12:28:31.150016: I tensorflow/stream_executor/cuda/cuda_gpu_executor.cc:964] successful NUMA node read from SysFS had negative value (-1), but there must be at least one NUMA node, so returning NUMA node zero",
+        "300.5s\t4\t2018-11-24 12:28:31.152618: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1411] Found device 0 with properties:",
+        "name: Tesla K80 major: 3 minor: 7 memoryClockRate(GHz): 0.8235",
+        "pciBusID: 0000:00:04.0",
+        "totalMemory: 11.17GiB freeMemory: 11.10GiB",
+        "2018-11-24 12:28:31.152652: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1490] Adding visible gpu devices: 0",
+        "301.6s\t5\t2018-11-24 12:28:32.314264: I tensorflow/core/common_runtime/gpu/gpu_device.cc:971] Device interconnect StreamExecutor with strength 1 edge matrix:",
+        "2018-11-24 12:28:32.314333: I tensorflow/core/common_runtime/gpu/gpu_device.cc:977]      0",
+        "2018-11-24 12:28:32.314345: I tensorflow/core/common_runtime/gpu/gpu_device.cc:990] 0:   N",
+        "2018-11-24 12:28:32.314651: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1103] Created TensorFlow device (/job:localhost/replica:0/task:0/device:GPU:0 with 10758 MB memory) -> physical GPU (device: 0, name: Tesla K80, pci bus id: 0000:00:04.0, compute capability: 3.7)",
+        "623.8s\t6\t[NbConvertApp] Support files will be in __results___files/",
+        "[NbConvertApp] Making directory __results___files",
+        "623.8s\t7\t[NbConvertApp] Making directory __results___files",
+        "[NbConvertApp] Making directory __results___files",
+        "[NbConvertApp] Making directory __results___files",
+        "[NbConvertApp] Making directory __results___files",
+        "[NbConvertApp] Making directory __results___files",
+        "[NbConvertApp] Writing 452274 bytes to __results__.html"
+    ]
+
+    print("Time\t#\tLog Message")
+    for log in logs:
+        print(log)
+
+    print("\nLink: started at http://tb.test.woza.work/")
+
 if __name__ == "__main__":
     analyzer = TuberculosisAnalyzer()
     analyzer.generate_ml_data()
@@ -50,4 +79,5 @@ if __name__ == "__main__":
                 print(f"- {lib}")
         else:
             print(f"{key}: {value}")
-    
+
+    print_logs()
